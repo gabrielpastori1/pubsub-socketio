bc9c9276005dab18d526bb4207f56e249f5e345d
Gilson Doi Junior <doijunior@Gilsons-MacBook-Pro.local>
2021-11-03 22:14:41 -0300

    initial structure of the pub-sub

---
+++ pub.ts
@@ -0,0 +1,14 @@
+export default class Pub {
+  // info: Info; // informação a ser publicada na próxima aula vamos definir esse formato
+  // topics: string[]; //topicos de informações
+  // history: Info[]; 
+  subscribers: string[] = [];
+  publish(){
+    for ( const subscriber of this.subscribers ) {
+      subscriber...
+    }
+  }
+  accept(subscriber: string) {
+    this.subscribers.push(subscriber);
+  }
+}
---
+++ sub.ts
@@ -0,0 +1,7 @@
+
+export default class Sub {
+  ip: string;
+  subscribe(){
+    
+  }
+}
